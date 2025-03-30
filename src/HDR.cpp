552bee177bafe69e32cdd41153dc6e6755334e3d
Kristan Schlikow <kristian@schlikow.de>
2025-03-30 04:34:42 +0200

    chore: Forgot to save the color rotation for HDR

--- src/HDR.cpp
+++ src/HDR.cpp
@@ -20,7 +20,8 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(
 	dxOperator,
 	dxTransferFunction,
 	dxExposure,
-	dxPaperWhite);
+	dxPaperWhite,
+	dxColorRotation);
 
 void HDR::DrawSettings()
 {
