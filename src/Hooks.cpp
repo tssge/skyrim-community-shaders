811dd5f1443d82ffdcd6a5ff3d1c887f7f9d4993
tssge <tssge@users.noreply.github.com>
2025-05-22 15:40:57 +0000

    style: 🎨 apply clang-format changes

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -461,8 +461,7 @@ HRESULT WINAPI hk_D3D11CreateDeviceAndSwapChain(
 		logger::info("[Hooks] Setting swapchain colorspace and HDR metadata");
 		IDXGISwapChain4* swapChain4 = nullptr;
 		if (SUCCEEDED((*ppSwapChain)->QueryInterface(IID_PPV_ARGS(&swapChain4)))) {
-
-			if(!shouldProxy) {
+			if (!shouldProxy) {
 				// Set DX11 HDR colorspace only when DX12 proxy is not in use
 				//
 				// Otherwise, if both swapchains have PQ set, double PQ transformation will happen
