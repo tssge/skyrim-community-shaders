79936355d5e1a7a438af5e3129d215ba1acb9709
doodlum <15017472+doodlum@users.noreply.github.com>
2025-07-14 01:18:52 +0100

    fix: always update eye position (#1253)

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -1131,6 +1131,16 @@ namespace Hooks
 			stl::write_thunk_call<Main_Update_Begin>(REL::RelocationID(35565, 36564).address() + REL::Relocate(0x53, 0x6E));
 			stl::write_thunk_call<Main_Update_Swap>(REL::RelocationID(35565, 36564).address() + REL::Relocate(0x5D2, 0xA97));
 		}
+
+		// Patch eye position in BSLightingShader::SetupGeometry to always update due to additional effects which may require it
+		// The variable updateEyePosition is set to false by default. By patching to be true it will always update the eye position
+		// SE and AE use a 7-byte instruction whereas VR uses a 8-byte instruction
+		// We offset from the base address of the containing function to the instruction itself and then to the value the instruction sets
+		{
+			logger::info("Patching BSLightingShader::SetupGeometry::updateEyePosition");
+			uintptr_t setupGeometryUpdateEyePosition = REL::RelocationID(100565, 107300).address() + REL::Relocate(0x50, 0x75, 0x78);
+			REL::safe_write(setupGeometryUpdateEyePosition + REL::Relocate(6, 6, 7), bool{ true });
+		}
 	}
 
 	/**
